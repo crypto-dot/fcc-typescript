99af36d6665930f17824d6f8ae75df34bc8e86ae
Carlos Arbizu <carlos2345532@gmail.com>
2022-12-02 01:47:48 -0600

    functions video section and code finished

--- 02Basics/myFunction.ts
+++ 02Basics/myFunction.ts
@@ -12,4 +12,17 @@ let loginUser = (name: string, email: string, isPaid: boolean = false) => {
 
 }
 
+const heros = ["Hero", "Thor", "Zeus"];
+// const heros = [1,2,3]
+//We can rely on typescript to switch context depending on the datatypes in the array
+
+//We can and should specify what our map callback functions return
+heros.map((hero): string => {
+    return hero + ' is cool!';
+});
+
+function fail(msg: string): never {
+    throw new Error(msg);
+}
+
 export { }
